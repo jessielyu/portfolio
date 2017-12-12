6cb9f9193aead242dfe1a18e1ea1cac06428aad9
Jessie Lyu <jessielyu@Jessies-MacBook-Pro.local>
2017-12-11 20:56:16 -0800

    version 3.0

---
+++ portfolio2_0/js/core.js
@@ -0,0 +1,19 @@
+var template = {
+  Modal:'<div class="col-3"><a href=""><p></p><img src=""></a></div>'
+};
+
+function compileTemplate() {
+
+}
+
+function init() {
+    var modalList = {
+      facepalm: {
+        name: 'FACEPALM(打脸)',
+        image: 'image/portfolio/dalianApp/thumbnail.png'
+      },
+      shopping: {},
+      habitTracker: {},
+      smartLock:{}
+    };
+}
