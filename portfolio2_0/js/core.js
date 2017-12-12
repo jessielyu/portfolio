2e193a1c80bbc4e07354b0a66111c5fcbc769834
Jessie Lyu <jessielyu@Jessies-MacBook-Pro.local>
2017-12-12 00:12:33 -0800

    portfolio page

--- portfolio2_0/js/core.js
+++ portfolio2_0/js/core.js
@@ -1,19 +1,76 @@
 var template = {
-  Modal:'<div class="col-3"><a href=""><p></p><img src=""></a></div>'
+    Modal:'<div id="portfolio">{{#each this}}<div class="col-2 portfolio-item"><a href="{{this.href}}" class="portfolio-link"><div class="caption"><p>{{this.name}}</p></div><img src="{{this.image}}" alt="{{this.name}}"></a></div>{{/each}}</div>'
+};
+
+var modalList = {
+    facepalm: {
+        name: 'FACEPALM(打脸)',
+        image: 'image/portfolio/thumbnail/dalian.png',
+        href:'#dalian'
+    },
+    shopping: {
+        name: 'LIVE SHOPPING(购物直播)',
+        image: 'image/portfolio/thumbnail/shoppingLive.png',
+        href:'#shoppingLive'
+    },
+    habitTracker: {
+        name: 'HABIT TRACKER',
+        image: 'image/portfolio/thumbnail/habitTracker.jpg',
+        href:'#habitTracker'
+    },
+    smartLock:{
+        name: 'SMART LOCKER',
+        image: 'image/portfolio/thumbnail/smartLock.jpg',
+        href: '#smartLock'
+    },
+    educationGame:{
+        name: 'EDUCATION GAMES',
+        image: 'image/portfolio/thumbnail/educationGame.png',
+        href: '#educationGame'
+    },
+    iEar:{
+        name: 'IEAR',
+        image: 'image/placeholder.png',
+        href: '#iEar'
+    },
+    movieLog:{
+        name: 'MOVIE LOG',
+        image: 'image/portfolio/thumbnail/movieLog.png',
+        href: '#movieLog'
+    },
+    lait:{
+        name: 'LAIT',
+        image: 'image/placeholder.png',
+        href: '#lait'
+    },
+    scratch:{
+        name: 'SCRATCH',
+        image: 'image/portfolio/thumbnail/scratch.jpg',
+        href: '#scratch'
+    },
+    graphicDesign:{
+        name: 'GRAPHIC DESIGN',
+        image: 'image/portfolio/thumbnail/graphicDesign.png',
+        href: '#graphicDesign'
+    },
+    photography:{
+        name: 'PHOTOGRAPHY',
+        image: 'image/portfolio/thumbnail/photography.jpg',
+        href: '#photography'
+    }
 };
 
 function compileTemplate() {
-
+    for (var key in template) {
+        template[key] = Handlebars.compile(template[key]);
+    }
 }
 
 function init() {
-    var modalList = {
-      facepalm: {
-        name: 'FACEPALM(打脸)',
-        image: 'image/portfolio/dalianApp/thumbnail.png'
-      },
-      shopping: {},
-      habitTracker: {},
-      smartLock:{}
-    };
+    $('#content').html(template.Modal(modalList));
 }
+
+window.onload = function () {
+    compileTemplate();
+    init();
+};
